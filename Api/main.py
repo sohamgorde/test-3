7717d54e62aeb4b70ec98e4e71a40f276f085a91
sohamgorde <soham1gorde@gmail.com>
2025-10-15 19:18:57 -0400

    change 1

---
+++ Api/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastApi
+
+app = fastapi(
+    title = "fast api example"
+    description = "this is an example of using fast api"
+)
+
+@app.get('/')
+
+def default_route():
+    return "you have reached the default route back end sever is lisening "
